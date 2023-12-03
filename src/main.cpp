01acae93d325ea6fa1a845f4ec24a572ddf84d62
blu133721 <tiganinpungasrl@gmail.com>
2023-12-03 20:24:53 +0200

    First compile

---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <swiftly/swiftly.h>
+#include <swiftly/server.h>
+#include <swiftly/database.h>
+#include <swiftly/commands.h>
+#include <swiftly/configuration.h>
+#include <swiftly/logger.h>
+#include <swiftly/timers.h>
+
+Server *server = nullptr;
+PlayerManager *g_playerManager = nullptr;
+Database *db = nullptr;
+Commands *commands = nullptr;
+Configuration *config = nullptr;
+Logger *logger = nullptr;
+Timers *timers = nullptr;
+
+void OnProgramLoad(const char *pluginName, const char *mainFilePath)
+{
+    Swiftly_Setup(pluginName, mainFilePath);
+
+    server = new Server();
+    g_playerManager = new PlayerManager();
+    commands = new Commands(pluginName);
+    config = new Configuration();
+    logger = new Logger(mainFilePath, pluginName);
+    timers = new Timers();
+}
+
+void OnPluginStart()
+{
+}
+
+void OnPluginStop()
+{
+    timers->UnregisterTimers();
+}
+
+const char *GetPluginAuthor()
+{
+    return "";
+}
+
+const char *GetPluginVersion()
+{
+    return "1.0.0";
+}
+
+const char *GetPluginName()
+{
+    return "swiftly_afk";
+}
+
+const char *GetPluginWebsite()
+{
+    return "";
+}
